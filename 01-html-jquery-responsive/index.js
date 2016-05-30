3711ca240280c35e09ef55f23dea97d97995ae78
Vasil Karpachev <v.karpachev@gmail.com>
2016-05-30 09:14:42 +0300

    compression added

--- 01-html-jquery-responsive/index.js
+++ 01-html-jquery-responsive/index.js
@@ -2,9 +2,11 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 var path = require("path");
+var compression = require('compression');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(compression());
 
 
 
@@ -21,7 +23,7 @@ app.get("/login", function(req,res) {
 	res.render('login', {title:"Login"});
 });
 
-app.use( express.static("public") );
+app.use( express.static("public", { maxAge: 31557600000 }) );
 
 app.listen(80, function(){
 	console.log("Listening for static files");
